0dfb4e33fafe443d5766bff55ececf51ac869e59
Leon Hlabathi <114271403+itsleonbro@users.noreply.github.com>
2024-09-06 22:44:55 +0200

    add emoji display function to reflect score comparison between home and guest

--- js/script.js
+++ js/script.js
@@ -1,32 +1,55 @@
 const scoreHome = document.getElementById("score-home");
 const scoreGuest = document.getElementById("score-guest");
-const homeDisplay = document.getElementById("home-score-display");
+const homeEmoji = document.getElementById("home-emoji");
+const guestEmoji = document.getElementById("guest-emoji");
 
 const addHomeOne = () => {
   scoreHome.textContent = parseInt(scoreHome.textContent) + 1;
+  displayEmoji();
 };
 
 const addHomeTwo = () => {
   scoreHome.textContent = parseInt(scoreHome.textContent) + 2;
+  displayEmoji();
 };
 
 const addHomeThree = () => {
   scoreHome.textContent = parseInt(scoreHome.textContent) + 3;
+  displayEmoji();
 };
 
 const addGuestOne = () => {
   scoreGuest.textContent = parseInt(scoreGuest.textContent) + 1;
+  displayEmoji();
 };
 
 const addGuestTwo = () => {
   scoreGuest.textContent = parseInt(scoreGuest.textContent) + 2;
+  displayEmoji();
 };
 
 const addGuestThree = () => {
   scoreGuest.textContent = parseInt(scoreGuest.textContent) + 3;
+  displayEmoji();
 };
 
 const newGame = () => {
   scoreHome.textContent = 0;
   scoreGuest.textContent = 0;
+  displayEmoji();
+};
+
+const displayEmoji = () => {
+  homeEmoji.textContent = "😄";
+  guestEmoji.textContent = "😄";
+
+  if (parseInt(scoreHome.textContent) > parseInt(scoreGuest.textContent)) {
+    homeEmoji.textContent = "😄";
+    guestEmoji.textContent = "😔";
+  } else if (
+    parseInt(scoreHome.textContent) < parseInt(scoreGuest.textContent)
+  ) {
+    homeEmoji.textContent = "😔";
+    guestEmoji.textContent = "😄";
+  }
 };
