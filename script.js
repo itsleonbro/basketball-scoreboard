c78bfbd0b9f16eb8693a52c9b1a6c9d1bbaa5da8
Leon Hlabathi <114271403+itsleonbro@users.noreply.github.com>
2024-09-06 20:04:17 +0200

    added score increment functionality

---
+++ script.js
@@ -0,0 +1,26 @@
+const scoreHome = document.getElementById("score-home");
+const scoreGuest = document.getElementById("score-guest");
+
+const addHomeOne = () => {
+  scoreHome.textContent = parseInt(scoreHome.textContent) + 1;
+};
+
+const addHomeTwo = () => {
+  scoreHome.textContent = parseInt(scoreHome.textContent) + 2;
+};
+
+const addHomeThree = () => {
+  scoreHome.textContent = parseInt(scoreHome.textContent) + 3;
+};
+
+const addGuestOne = () => {
+  scoreGuest.textContent = parseInt(scoreGuest.textContent) + 1;
+};
+
+const addGuestTwo = () => {
+  scoreGuest.textContent = parseInt(scoreGuest.textContent) + 2;
+};
+
+const addGuestThree = () => {
+  scoreGuest.textContent = parseInt(scoreGuest.textContent) + 3;
+};
